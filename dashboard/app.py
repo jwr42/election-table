e4f5d95aa2e888e266bd2418bbdce55ed51b7de9
Jonathan Roberts <jonathanroberts@Jonathans-MacBook-Air.local>
2024-07-31 15:07:03 +0100

    updated sidebar

--- dashboard/app.py
+++ dashboard/app.py
@@ -10,13 +10,15 @@ from shared import app_dir, df
 
 ui.page_opts(title="UK General Election Results", fillable=True)
 
-with ui.sidebar(title="Filter controls"):
+with ui.sidebar(title="Filter Controls"):
     ui.input_checkbox_group(
-        "commons_speaker",
-        "Standing as Common's Speaker",
-        {"True": True, "False": False},
-        selected=["True", "False"],
+        "year",
+        "Select Elections",
+        {"2024": "", "2019": "", "2017": ""},
+        selected=["2024", "2019"],
     )
+    ui.input_selectize("constituencies", "Select Constituencies", {}, selected=[])
+    ui.input_switch("speaker", "Include Speaker", value=True)
 
 
 with ui.layout_column_wrap(fill=False):
@@ -55,9 +57,9 @@ with ui.layout_columns():
     with ui.card(full_screen=True):
         ui.card_header("UK General Election Candidates")
 
-        @render.table
-        def _():
-            return pd.DataFrame(filtered_df())
+        @render.data_frame
+        def table():
+            return render.DataGrid(filtered_df())
 
 
 ui.include_css(app_dir / "styles.css")
@@ -65,36 +67,33 @@ ui.include_css(app_dir / "styles.css")
 
 @reactive.calc
 def filtered_df():
-    cols = [
-        "General election polling date",
-        "Constituency name",
-        "Candidate result position",
-        "Majority",
-        "Candidate family name",
-        "Candidate given name",
-        "Main party abbreviation",
-        "Candidate vote count",
-        "Candidate vote share",
-    ]
-
-    filt_df = df[cols]
-
-    filt_df.rename(
-        columns={
-            "General election polling date": "Date",
-            "Constituency name": "Constituency",
-            "Candidate result position": "Result Position",
-            "Majority": "Majority",
-            "Candidate family name": "Surname",
-            "Candidate given name": "First Name",
-            "Main party abbreviation": "Party",
-            "Candidate vote count": "Vote Count",
-            "Candidate vote share": "Vote Share",
-        },
-        inplace=True,
+    filt_df = (
+        df[
+            [
+                "General election polling date",
+                "Constituency name",
+                "Candidate result position",
+                "Majority",
+                "Candidate family name",
+                "Candidate given name",
+                "Main party abbreviation",
+                "Candidate vote count",
+                "Candidate vote share",
+            ]
+        ].rename(
+            columns={
+                "General election polling date": "Date",
+                "Constituency name": "Constituency",
+                "Candidate result position": "Result Position",
+                "Majority": "Majority",
+                "Candidate family name": "Surname",
+                "Candidate given name": "First Name",
+                "Main party abbreviation": "Party",
+                "Candidate vote count": "Vote Count",
+                "Candidate vote share": "Vote Share",
+            }
+        )
+        # .fillna("")
     )
-
-    filt_df = filt_df.fillna("")
-
     # filt_df = filt_df[filt_df["Candidate is standing as Commons Speaker"].isin(input.commons_speaker())]
     return filt_df
